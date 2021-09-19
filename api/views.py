a1854376211349fa6b96d6500174cda231929f9b
kohishibashi <trendy8455@gmail.com>
2021-09-19 18:35:03 +0900

    modify: can get query_params

--- api/views.py
+++ api/views.py
@@ -4,4 +4,17 @@ from rest_framework.response import Response
 
 class Hello(APIView):
     def get(self, request, format=None):
-        return Response('Hello World!')
+        num = request.query_params.get('num')
+        res_dict = {
+            'greet': '' 
+        }
+        if num:
+            count = int(num)
+            res_dict['greet_list'] = []
+            for i in range(count):
+                res_dict['greet'] += 'Hello World!'
+                res_dict['greet_list'] += ['Hello World!']
+            res_dict['num'] = count
+        else:
+            res_dict['greet'] = 'Hello World!'        
+        return Response(res_dict)
